fc20073f5c318adfd2ad9a538655729833b61f62
Siddartha pogula <pogulaSiddharth96@gmail.com>
2024-02-27 15:34:02 +0530

    done

---
+++ src/components/Chip.js
@@ -0,0 +1,126 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { users } from '../constants';
+import bg from "../utils/benner_11.jpg"
+
+const Chip = () => {
+  const [searchText, setSearchText] = useState("");
+  const [selectedUsers, setSelectedUsers] = useState([]);
+  const [allUsers, setAllUsers] = useState(users);
+  const [findUsers, setFindUsers] = useState(allUsers);
+  const [usersVisibility, setUsersVisibility] = useState(false);
+  const userScrollRef = useRef(null);
+  const inputRef = useRef(null);
+
+
+  function handleClick(clickedUser) {
+    setSelectedUsers((prevUsers) => {
+      const isUserExists = prevUsers.some((user) => user.id === clickedUser.id);
+      if(isUserExists){
+        return prevUsers.filter((user)=>user.id !== clickedUser.id);
+      }
+     const newUsers = findUsers.filter((user) => user?.id !== clickedUser?.id);
+      setFindUsers(newUsers);
+      setSearchText("");
+      return [...prevUsers, clickedUser];
+    });
+  }
+  function removeUser(clickedUser){
+    const remainingUsers = selectedUsers.filter((user)=>user?.id !== clickedUser?.id);
+    const isUserExistsInAllusers = findUsers.some((user)=>user?.id === clickedUser?.id);
+    if(!isUserExistsInAllusers){
+      setFindUsers((prevUsers)=>[...prevUsers, clickedUser]);
+    }
+    setSelectedUsers(remainingUsers);
+  }
+
+  function handleInput(e) {
+    const value = e.target.value;
+    setSearchText(value)
+    setUsersVisibility(true);
+  }
+  function handleFocus (){
+    setUsersVisibility(true);
+  }
+  useEffect(() => {
+    console.log(searchText);
+    searchUsers();
+  }, [searchText]);
+
+   useEffect(() => {
+    const handleClickOutside = (event) => {
+         if (userScrollRef.current && inputRef.current && !userScrollRef.current.contains(event.target) && !inputRef.current.contains(event.target) ) {
+             setUsersVisibility(false);
+    }
+    };
+  
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [userScrollRef, inputRef]);
+  
+  function searchUsers() {
+    const lowerCaseSearchText = searchText.toLowerCase();
+    const searchedUsers = allUsers.filter(
+      (user) => user.name.toLowerCase().includes(lowerCaseSearchText) && !selectedUsers.includes(user)
+    );
+    setFindUsers(searchedUsers);
+  }
+  
+
+  return (
+    <div >
+    <div className=" -z-10 absolute ">
+        <img src={bg }alt="bg-theme" /> 
+    </div>      
+    <div  className= ' z-20  flex justify-center mt-[5rem]'>
+    <div
+  onFocus={() => setUsersVisibility(true)}
+  className='flex flex-wrap w-[90%] md:w-[80%] lg:w-[65%] text-lg p-2 rounded-[5px] focus:outline-none px-3 border-[1px] border-gray-300 transition-all duration-300 hover:shadow-md focus:shadow-outline'
+>
+        {selectedUsers.length !== 0 ? selectedUsers.map((user)=>{
+          return (
+            <div  key={user.id} className=' flex items-center px-1 py-1  rounded-lg my-1 bg-slate-200 mr-1'>
+          <img className=' w-[0.8rem] rounded-full h-[0.8rem] mr-1' src= {user.profile}/>
+          <span className=' text-xs text-gray-700'>{user.name}</span>
+          <button onClick={()=>{
+            removeUser(user)
+          }} className=' mx-1 px-[0.3rem]  text-xs bg-slate-400 rounded-full '>x</button>
+          </div>)
+        }) : <></>}
+
+        <input
+          ref={ inputRef}
+          value={searchText}
+          onFocus={handleFocus}
+          onChange={(e) =>handleInput(e)}
+          className='outline-none border-none w-full md:w-4/5 lg:w-1/3 '
+          placeholder='Search by email or name'/>
+
+          </div>
+        </div>
+     { usersVisibility && <div
+      ref={userScrollRef}
+     className='
+     transition-all duration-200 hover:shadow-md focus:shadow-outline
+     bg-white opacity-80 ml-[2rem] lg:ml-[14rem] md:ml-[4.7rem]   rounded-md w-[85%] md:w-[40%] lg:w-[30%] max-h-[25rem] overflow-y-scroll mt-2 '>
+      {findUsers.map((user) => (
+        <div className='
+        transition-all duration-200 hover:shadow-md focus:shadow-outline rounded-sm
+        flex items-center py-2 px-1 gap-1 cursor-pointer' 
+          onClick={()=>{handleClick(user)
+          }}
+          key={user.id}>
+        <img className=' rounded-full w-[2.2rem] h-[2.2rem]' src={user.profile} alt={user.name} />
+        <h1 className= ' font-semibold  '>{user.name}</h1>
+        <h1 className=' text-xs  text-gray-500 '>{user.email}</h1>
+        </div>
+      ))}
+      </div> }
+      
+    </div>
+    
+  );
+};
+
+export default Chip;
---
+++ src/constants.js
@@ -0,0 +1,106 @@
+ export const users = [
+        { id: 1,
+          name: "Aarav Sharma",
+          email: "AaravSharma@example.com",
+          profile : "https://www.ninjaonlinedating.com/blog/wp-content/uploads/2019/08/SmileModifiedKRAK.jpg"
+        },
+        { id: 2,
+          name: "Sofia Patel",
+          email: "SofiaPatel@example.com",
+          profile : "https://i.pinimg.com/originals/9e/30/d6/9e30d6511826ff036fb2a7f70eb98525.jpg"
+        },
+        { id: 3,
+          name: "Arjun Kumar",
+          email: "ArjunKumar@example.com",
+          profile : "https://th.bing.com/th/id/OIP.Ib5gIJSLCo1Awh4V16WW6wHaHa?pid=ImgDet&w=199&h=199&c=7&dpr=1.5"
+        },
+        { id: 4,
+          name: "Aisha Singh",
+          email: "AishaSingh@example.com",
+          profile : "https://www.best4geeks.com/wp-content/uploads/2018/07/30-cute-girl-facebook-profile-picture-1-1803x2048.jpg"
+        },
+        { id: 5,
+          name: "Vivaan Chopra",
+          email: "VivaanChopra@example.com",
+          profile :  'https://media.licdn.com/dms/image/D4E03AQH5BU_foKqPBA/profile-displayphoto-shrink_800_800/0/1684767481015?e=2147483647&v=beta&t=rtdzT-dN4iWnP2De4pW2xcbSbKtZPIXFG06Wx-Big1I'
+        },
+        { id: 6,
+          name: "Zara Joshi",
+          email: "ZaraJoshi@example.com",
+          profile : "https://www.best4geeks.com/wp-content/uploads/2018/07/30-cute-girl-facebook-profile-picture-1-1803x2048.jpg"
+        },
+        { id: 7,
+          name: "Advait Gupta",
+          email: "AdvaitGupta@example.com",
+          profile : "https://media.licdn.com/dms/image/D4E03AQHyrLqTthOm9A/profile-displayphoto-shrink_800_800/0/1680923000881?e=2147483647&v=beta&t=KysJGLfO4YkWFkyD33Nb-zR6APmFq6LAJiLctBEQQs0"
+        },
+        { id: 8,
+          name: "Anaya Malhotra",
+          email : 'AnayaMalhotra@example.com',
+          profile :  "https://th.bing.com/th/id/R.8f99be5401a98639cf1f1d6b90a3c0fa?rik=HlnBzKKIOnofqA&riu=http%3a%2f%2fbestprofilepix.com%2fwp-content%2fuploads%2f2014%2f03%2fcute-stylish-winter-girls-facebook-profile-pictures.jpg&ehk=cim2HF88p9d03c%2bZvp7LzXLZ4%2bnC6dbZkB03zLTuoiI%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1", 
+        },
+        { id: 9,
+          name: "Kabir Verma",
+          email: "KabirVerma@example.com",
+          profile : "https://ucy-compsci.org/wp-content/uploads/2020/06/DSC_0774_crop-1536x1536.jpg"
+        },
+        { id: 10,
+          name: "Ishita Bansal",
+          email: "IshitaBansal@example.com",
+          profile : "https://i.pinimg.com/originals/4c/c2/b6/4cc2b67bf86989e1f940fd0ea6ba5f19.jpg"
+        },
+        { id: 11,
+          name: "Vihaan Rao" ,
+          email: "VihaanRao@example.com",
+          profile : "https://www.over40datingsite.co.uk/wp-content/uploads/2016/09/shutterstock_341919467.jpg"
+        },
+        { id: 12,
+          name: "Aaradhya Khanna" ,
+          email: "AaradhyaKhanna@example.com",
+          profile : "https://th.bing.com/th/id/OIP.qob20kwKwnSKq1YuNJY7ZwAAAA?w=400&h=600&rs=1&pid=ImgDetMain"
+        },
+        { id: 13,
+          name: "Reyansh Sinha",
+          email: "ReyanshSinha@example.com",
+          profile :  "https://music.utexas.edu/sites/bsom/files/styles/utexas_image_style_500w_500h/public/2020-09/Duke_Robert.jpg?itok=5dtJyhz5"
+        },
+        { id: 14,
+          name: "Amara Mittal",
+          email: "AmaraMittal@example.com",
+          profile : "https://i.pinimg.com/originals/66/43/f6/6643f6c4e45cb63fdf2e97e5f546027c.jpg"
+        },
+        { id: 15,
+          name: "Aryan Shah" ,
+          email: "AryanShah@example.com",
+          profile : "https://marketing.webassets.siemens-healthineers.com/f1b6942264e6fb15/a6f40b1caec2/v/9c7eb09445e2/Executive-Summit_Halamka.jpg"
+        },
+        { id: 16,
+          name: "Sanaya Rastogi",
+          email: "SanayaRastogi@example.com",
+          profile : "https://th.bing.com/th/id/OIP.5jvBOzRy9raCxE8_JCQpmAAAAA?pid=ImgDet&w=199&h=264&c=7&dpr=1.5"
+        },
+        { id: 17,
+          name: "Rohan Agarwal",
+          email: "RohanAgarwal@example.com",
+          profile : "https://th.bing.com/th/id/OIP.wpnRXyPbeNXZn1-WoieXagHaHa?w=2500&h=2500&rs=1&pid=ImgDetMain"
+        },
+        { id: 18,
+          name: "Siya Tiwari",
+          email: "SiyaTiwari@example.com",
+          profile : "https://th.bing.com/th/id/OIP.dJmIG0SIzO8ZDVUxrF8yzQAAAA?pid=ImgDet&w=199&h=298&c=7&dpr=1.5"
+        },
+        { id: 19,
+          name: "Karan Nair",
+          email: "KaranNair@example.com",
+          profile : "https://pt.ivao.aero/portal/wp-content/uploads/2020/06/Marco_Verdial_marcoverdial_fotos_e_videos_do_4.png"
+        },
+        { id: 20,
+          name: "Riya Saxena",
+          email: "RiyaSaxena@example.com",
+          profile : "https://th.bing.com/th/id/OIP.WKo79DAEs34CQMov85FNBAHaKb?w=1000&h=1409&rs=1&pid=ImgDetMain"
+        },
+]
+    
+
+
+
